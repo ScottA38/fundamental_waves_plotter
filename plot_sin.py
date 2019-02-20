19795f31d14322cc1354487cd894e5d2a1af040e
ScottA94 <S192660@uos.ac.uk>
2019-02-20 13:14:14 +0000

    committing initial work. Previously this program could plot waves individually but I am approximately 80% through the process of reordering the code to allow for a single compound plot of all of the waves

---
+++ plot_sin.py
@@ -0,0 +1,181 @@
+"""aim:
+to generate a Python program which maps the individual fundamental waveforms (harmonics not yet considered) for the
+intro riff on lead guitar to Queens of the Stone Age - Better Living Through Chemistry.
+
+problems:
+    -Plotting a wave visually
+        -using matplotlib library
+    |Working out time intervals between notes in the song and mapping them in terms of time
+        |how many beats go into a second of music? Or should it be the inverse - how many seconds' duration is a 4-beat bar?
+    |Discerning what the frequency of each individual note played is
+        |online resources can be used to discern the fundamental wave frequency for a specific note played
+    -Combining ranges of y values to make combined wave shapes
+        -offsetting a specific range a certain number of zeros such that the wave starts at the correct point on the x-axis
+        -Python has built-in functionality to add lists together
+    -Designing dataset to input into program via argv
+        |using .json format
+        -what fields should be used? (current: "frequency", "note", "start", "end", where "start" and "end are numbers as multiple of time-per-beat")
+        -ensure that program treats an "end" field as terminating just before the start of the actual beat which is shown
+        -ensure notes do not end until that note is unfretted, muted or fret is changed in the song, and not before
+"""
+import numpy
+from numpy import pi
+import matplotlib.pyplot as plt
+from sys import argv
+import json
+import pprint
+import collections
+import math
+from random import choice
+
+#setup vars etc.
+#taking input file from user
+script, json_in = argv
+
+pp = pprint.PrettyPrinter(indent=4)
+
+#end setup
+
+class Sequence(dict):
+    resolution = 1000
+    def __init__(self, notes):
+        """initialise a wave sequence based upon a iterable sequence of Note instances"""
+        self.start, self.end, self.min_freq, self.max_freq = self._define_parameters(notes)
+        assert float(self.start) != float('inf'), "Sequence.__init__() has failed"
+        assert self.end != -1, "Sequence.__init__() has failed"
+        assert float(self.min_freq) != float('inf'), "Sequence.__init__() has failed, min_freq: {}".format(self.min_freq)
+        assert self.max_freq != -1, "Sequence.__init__() has failed"
+        #popualate the dict with time points as the keys
+        self.fromkeys(numpy.arange(self.start, self.end, 0.5), [])
+        for note in notes:
+            assert isinstance(note, Note) "sequence contains members which are not an instance of 'Note'"
+            #assumes that the bars are counted in 'eighth notes' and so for a 4-beat bar there are are 8 notes, hence the '0.5' interval
+            for beat in numpy.arange(note.start, note.end, 0.5):
+                self[beat].append(note.frequency)
+
+    def _calibrate_fig(self):
+        """function to set appropriate settings for matplotlib figure"""
+        self.fig = plt.figure()
+        self.fig.set_dpi(100)
+        plt.style.use('dark_background')
+        plt.grid(True, which="both", axis="both", color="w", linewidth="1")
+        #divide the x axis plot labels up by a user-defined system (numpy.arange(<start>, <stop>, <step>) -> range of all plot labels)
+        plt.xticks(numpy.linspace(riff.start, t_per_beat * riff.end, riff.end))
+
+    def _define_parameters(self, notes):
+        start = float('inf')
+        end = -1
+        min_freq = float('inf')
+        max_freq = -1
+        #look through the data given for the earliest start time of a wave and the latest temination of a wave, thus defining the total time range
+        #max_frequecncy and min_frequency are arbitrary and not required but are there to ensure that correct data is being disseminated correctly
+        for note in notes:
+            if note.frequency > max_freq:
+                max_freq = note.frequency
+            if note.frequency < min_freq:
+                min_freq = note.frequency
+            if note.start < start:
+                start = note.start
+            elif note.end > end:
+                end = note.end
+        return (start, end, min_freq, max_freq)
+
+    def plot_graph(self):
+        #make sure that required function has been fired upon instance
+        assert bool(self.x & self.y), "Sequence.plot_graph() has been called but Sequence.plot_axes has not been called on the instance."
+        #perform necessary setup functions for graph
+        self._calibrate_fig()
+        for note in self:
+            #create a range for a beat of 8 bars with 100 plotted points per second
+            x0 = numpy.arange(note.start, note.end, 0.001)
+            w = wave.waveGen(x0)
+            #plot a white line with the combined frequencies
+            plt.plot(x0, w, color="#FFFFFF")
+
+    def plot_axes(self):
+        self.x = numpy.arange(self.start, self.end, 0.01)
+        for interval in self:
+            ys = numpy.arange(self[interval], self[interval + 1], 0.01§
+            #index each individual frequency
+            for freq in self[interval]:
+                ys.append()
+
+class Note(object):
+    def __init__(self, frequency, note, start, end):
+        self.frequency, self.note, self.start, self.end = frequency, note, start, end
+        self.colour = gen_colour()
+
+    def waveGen(self, xRange):
+    """function to calculate all y values for a given input range of x-coordinates: (   y = f(x) = sin( xRange[i]*t )    )
+    frequency -> cycles per second of a wave
+    duration -> is the time duration of the entire input wave (end point relative to t = 0). Value will be a number as a multiple of t_per_beat, but must be a multiple of 0.25
+    xRange is an input of values to be plotted on the x-axis """
+        #instantiate list for return
+        a = []
+        assert self.frequency % 1 == 0, "Frequency argument for waveGen is not an integer value"
+        #need to determine closest s where s >= starting time and e where e <= ending time where s & e are points in xRange
+        for i in xRange:
+            value = sineWaveZero( self.frequency, i )
+            a.append(value)
+        return a
+
+def unwrap_json(json_obj):
+    self.notes = []
+    #perform sanity checks on the input argument to the system
+    assert json_data.__class__.__name__ == "list", "the data into the sequence is not of the correct type, it is of type '{}' needs to be of type 'list'".format(json_data.__class__.__name__)
+    intended_keys = [u"frequency", u"note", u"sections"]
+    for key in intended_keys:
+        assert key in list(d.viewkeys()), "list argument to NoteFactory does not have the correct keys.\nIt has: {}, but it should have: {}".format(list(d.viewkeys()), intended_keys)
+    #unwrap raw data parsed from JSON
+    for payload in json_data:
+        for interval in payload.sections:
+            notes.append(Note(payload.frequency, payload.note, interval["start"], interval["end"]))
+    return notes
+
+def sineWaveZero(w,t):
+    return numpy.sin(w * t)
+
+def gen_colour():
+    val_list = []
+    # range of all values for all hexadecimal digits
+    vals = range( 15 )
+    #cycle through random
+    for index in range(6):
+        new_value = choice(vals)
+        val_list.append( hex(new_value)[2:] )
+    return ("#" + "".join(val_list))
+
+assert json_in.endswith(".json"), "The input file to the program does not end with a '.json' file extension"
+
+#load JSON data into a Python dictionary
+data_in = {}
+with open(json_in, "r") as f:
+    data_in = json.loads(f.read())
+
+pp.pprint(data_in)
+
+notes = unwrap_json(data_in)
+#set up an instance with which to draw the graph
+riff = Sequence(notes)
+riff.
+riff.plot_graph()
+
+#display deduced total sequence information
+print("Information about the total {}".format(riff.__dict__))
+
+
+# actual_start = sequence["start"]
+# if sequence['start'] > 0:
+#     while actual_start == None:
+#         u_input = raw_input("Your audio sequence does not have musical information all the way to t=0, would you like to backfill to t = 0 with zeros?\t").lower()
+#         if u_input == "y":
+#             actual_start = 0
+#         elif u_input == "n":
+#             #exit out because the appropriate start point is already set
+#             break
+#
+#print("Actual start position is now:\t" + str(actual_start) + "(in beats per minute)")
+
+print("t_per_beat: %f, t_per_bar: %f" %(t_per_beat, t_per_bar))
+
+plt.show()
